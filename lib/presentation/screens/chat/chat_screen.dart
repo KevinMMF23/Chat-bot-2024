b6422481b392796da3c028ac2440650f222dcc6a
unknown <kevin>
2024-03-04 22:57:15 -0800

    proyecto version 2

---
+++ lib/presentation/screens/chat/chat_screen.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:yes_no_chat/presentation/screens/chat/widgets/my_message.bubble.dart';
+
+class ChatScreen extends StatelessWidget {
+  const ChatScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: const Padding(
+          padding: EdgeInsets.all(5),
+          child: CircleAvatar(
+            backgroundImage: NetworkImage(
+              'https://www.unotv.com/uploads/2022/07/dharius-091037.jpg',
+            ),
+          ),
+        ),
+        title: const Text("Dharius"),
+        centerTitle: false,
+      ),
+      body: MyWidget(),
+    );
+  }
+}
+
+class MyWidget extends StatelessWidget {
+  const MyWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 10),
+        child: Column(
+          children: [
+            Expanded(
+              child:ListView.builder(
+                itemCount: 101,
+                itemBuilder: ((context,index){
+                  return(index % ==0)
+                  ? const HerMessageBubble()
+                  : const MyMessageBubble();
+              })
+            )),
+            const Text("Hola"),
+            const Text("Hola2"),
+          ],
+        ),
+      ),
+    );
+  }
+}
