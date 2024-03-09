83d9edacfbb27146caa15317956fc1e7c8154c52
Kevin <a20490747@itmexicali.edu.mx>
2024-03-09 08:13:52 -0800

    prueba

--- lib/presentation/screens/shared/message_field_box.dart
+++ lib/presentation/screens/shared/message_field_box.dart
@@ -1,58 +1,53 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 
 class MessageFieldBox extends StatelessWidget {
   const MessageFieldBox({super.key});
 
-
   @override
   Widget build(BuildContext context) {
-    final TextEditingController=TextEditingController();
-    final enfoque = FocusNode();
+    final textController = TextEditingController();
+    final focusNode = FocusNode();
 
-
-    
-  final UnderlineInputBorder OutlineInputBorder= UnderlineInputBorder(
-      borderSide: const BorderSide(color: Colors.transparent),
-      borderRadius: BorderRadius.circular(20));
-    
-  final decoration = InputDecoration(
-      enabledBorder: OutlineInputBorder,
-      focusedBorder: OutlineInputBorder,
-      filled: true,
-      suffixIcon: IconButton(
-        icon: const Icon(Icons.send_and_archive_outlined),
-        onPressed: (){
-          final textValue = textcontroller.value.text;
-          print("impresion del boton de envio: $textValue");
-          textcontroler.clear();
-        },
-      ));
-      return TextFormField(
-            final enfoque = FocusNode();
-
-        controller: TextController,
-      onFieldSubmitted: (value){
-        print("es el submit valor: $value");
-      textcontroler.clear();
-      enfoque.clear();
-
+    return TextFormField(
+      controller: textController,
+      focusNode: focusNode,
+      onTapOutside: (event) {
+        focusNode.unfocus();
       },
-      decoration: decoration,
-      );
+      onFieldSubmitted: (value) {
+        print('Valor enviado: $value');
+        textController.clear();
+        focusNode.requestFocus();
+      },
+      decoration: _buildInputDecoration(
+        inputBorder: _outlineInputBorder(),
+        onPressed: () => _onPressed(textController: textController),
+      ),
+    );
   }
-  
-UnderlineInputBorder: OutlineInputBorder()=> UnderlineInputBorder(
-  borderSide: const BorderSide(color: Colors.transparent),
-  borderRadius: BorderRadius.circular(20));
 
-InputDecoration setDecoration({required inputBorder})=> InputDecoration(
-  enabledBorder: inputBorder,
-  focusedBorder: InputBorder,
-  filled:  true,
-  suffixIcon: IconButton(
-    
-  )
+  UnderlineInputBorder _outlineInputBorder() => const UnderlineInputBorder(
+        borderSide: BorderSide(color: Colors.transparent),
+        borderRadius: BorderRadius.all(Radius.circular(20)),
+      );
 
-)
+  InputDecoration _buildInputDecoration({
+    required InputBorder inputBorder,
+    required VoidCallback onPressed,
+  }) =>
+      InputDecoration(
+        enabledBorder: inputBorder,
+        focusedBorder: inputBorder,
+        filled: true,
+        suffixIcon: IconButton(
+          icon: const Icon(Icons.send_and_archive_outlined),
+          onPressed: onPressed,
+        ),
+      );
+
+  void _onPressed({required TextEditingController textController}) {
+    final textValue = textController.text;
+    print('Valor de la nueva función: $textValue');
+    textController.clear();
+  }
 }
