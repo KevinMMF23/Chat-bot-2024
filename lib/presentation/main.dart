99af477e5393a59063010c40d6301cfc00d1acfd
unknown <kevin>
2024-03-10 21:30:55 -0700

    Actualizacion del proyecto

--- lib/presentation/main.dart
+++ lib/presentation/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
+import 'package:provider/provider.dart';
 import 'package:yes_no_chat/config/theme/app_theme.dart';
+import 'package:yes_no_chat/presentation/providers/chat_provider.dart';
 import 'package:yes_no_chat/presentation/screens/chat/chat_screen.dart';
 
 void main() {
@@ -14,13 +15,13 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MultiProvider(
       providers: [
-        ChangeNotifierProvider(create: (_)=>ChatProvider()),
+        ChangeNotifierProvider(create: (_) => ChatProvider()),
       ],
       child: MaterialApp(
-      title: 'Yes  No App',
-      debugShowCheckedModeBanner: false,
-      theme: Apptheme(selectedColor: 4).theme(),
-      home: const ChatScreen()),
+          title: 'Yes No App',
+          debugShowCheckedModeBanner: false,
+          theme: Apptheme(selectedColor: 0).theme(),
+          home: const ChatScreen()),
     );
   }
 }
--- lib/presentation/widgets/her_message.bubble.dart
+++ lib/presentation/widgets/her_message.bubble.dart
@@ -7,27 +7,26 @@ class HerMessageBubble extends StatelessWidget {
   Widget build(BuildContext context) {
     final colors = Theme.of(context).colorScheme;
     return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         Container(
           decoration: BoxDecoration(
-            color: colors.primary,
-            borderRadius: BorderRadius.circular(20),
-          ),
+              color: colors.primary, borderRadius: BorderRadius.circular(20)),
           child: const Padding(
             padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
             child: Text(
-              "erwrqrrewrrewewrewrwe",
+              "Id fugiat incididunt aute ipsum nostrud ",
               style: TextStyle(color: Colors.white),
             ),
           ),
         ),
         const SizedBox(
-          height: 20
-          ),
-          const _ImageBubble(),
-          const SizedBox(
-            height: 10,
-          )
+          height: 10,
+        ),
+        const _ImageBubble(),
+        const SizedBox(
+          height: 10,
+        )
       ],
     );
   }
@@ -39,15 +38,13 @@ class _ImageBubble extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
-    print("ancho ${size.width}= ${size.width * 0.7}");
-    print("alto ${size.width}= ${size.width * 0.1}");
     return ClipRRect(
       borderRadius: BorderRadius.circular(20),
       child: Image.network(
           width: size.width * 0.7,
           height: size.height * 0.2,
           fit: BoxFit.cover,
-          "https://i.pinimg.com/originals/78/25/78/782578789c60ff95c9f9db701dfac609.gif",
+          "https://yesno.wtf/assets/no/14-cb78bf7104f848794808d61b9cd83eba.gif",
           loadingBuilder: (context, child, loadingProgress) =>
               (loadingProgress == null)
                   ? child
@@ -56,7 +53,7 @@ class _ImageBubble extends StatelessWidget {
                       height: size.height * 0.2,
                       padding: const EdgeInsets.symmetric(
                           horizontal: 10, vertical: 5),
-                      child: const Text("Cargando imagen ..."),
+                      child: const Text("cargando imagen ..."),
                     )),
     );
   }
