7d544bc0b88b374cc8c9cac7742276fbcd8d9adc
unknown <kevin>
2024-03-10 23:21:28 -0700

    se generan las posibles respuestas del bot

--- lib/presentation/providers/chat_provider.dart
+++ lib/presentation/providers/chat_provider.dart
@@ -1,18 +1,46 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:yes_no_chat/domain/entities/message.dart';
 
 class ChatProvider extends ChangeNotifier {
   List<Message> _messageList = [
-    Message(text: 'Hola mi vida', fromWho: FromWho.me),
-    Message(text: '¡Saca para los tacos!!!!', fromWho: FromWho.me),
-    Message(text: 'Nel pastel me debes una feria codo', fromWho: FromWho.hers),
-    Message(text: 'Pero te pongo las cocas', fromWho: FromWho.hers),
+    Message(text: 'Que tranza carnal, soy el rapero mxAleman', fromWho: FromWho.hers),
   ];
 
   List<Message> get messageList => _messageList;
 
   void addMessage(Message message) {
     _messageList.add(message);
+    
+    if (message.fromWho == FromWho.me) {
+      final response = generateResponse(message.text);
+      if (response != null) {
+        _messageList.add(response);
+      }
+    }
+
     notifyListeners();
+  }
+
+  Message? generateResponse(String text) {
+    final random = Random();
+    switch (text.toLowerCase()) {
+      case 'q tranza':
+        return Message(text: 'que once', fromWho: FromWho.hers);
+      case 'adios':
+        return Message(text: 'Adiós, ¡que tengas un buen día!', fromWho: FromWho.hers);
+      case '¿cómo estás?':
+        final responses = [
+          'Estoy bien, gracias por preguntar.',
+          'Me siento genial hoy.',
+          'Un poco cansada, pero bien en general.'
+        ];
+        final index = random.nextInt(responses.length);
+        return Message(text: responses[index], fromWho: FromWho.hers);
+      default:
+        // Puedes agregar más respuestas aquí o devolver null para no responder
+        return null;
+    }
   }
 }
